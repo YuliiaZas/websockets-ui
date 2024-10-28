d9956312c4fda34a2066f3195891f6c729310edf
OlegLepeshko <74596871+OlegLepeshko@users.noreply.github.com>
2024-10-28 20:10:17 +0300

    feat: fix index

--- src/http_server/handler/add_user_to_room.js
+++ src/http_server/handler/add_user_to_room.js
@@ -1,22 +1,4 @@
-// export function handleAddUserToRoom(ws, data, rooms ) {
-//     const room = rooms.find(room => room.roomId === data.indexRoom);
-//     if (room) {
-//       const player = { name: `Player${room.roomUsers.length + 1}`, password: '', index: room.roomUsers.length };
-//       room.roomUsers.push(player);
-//       if (room.roomUsers.length === 2) {
-//         const gameId = rooms.length;
-//         games[gameId] = {
-//           idGame: gameId,
-//           idPlayer: room.roomUsers.length,
-//           ships: []
-//         };
-//         // room.roomUsers.forEach((player, index) => {
-//         //   ws.send(JSON.stringify({ type: 'create_game', data: { idGame: gameId, idPlayer: index }, id: 0 }));
-//         // });
-//       }
-//       ws.send(JSON.stringify({ type: 'update_room', indexRoom: 0, id: 0 }));
-//     }
-//   };
+
 
 export function handleAddUserToRoom(ws, data, rooms) {
   // const roomId = data.indexRoom;
